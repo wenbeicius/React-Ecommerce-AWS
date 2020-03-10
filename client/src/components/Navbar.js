6b1cd65e057c5e3bdf3707a07ccf1498a0689bc2
wenbeicius <wenbei@gmail.com>
2020-03-10 12:51:00 -0700

    initial commit'

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Box, Text, Heading, Image } from "gestalt";
+import { NavLink } from 'react-router-dom';
+
+
+const Navbar = () => (
+    <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="around"
+        height={70}
+        color="midnight"
+        padding={1}
+        shape="roundedBottom"
+    >
+    {/* Sign In Link */}
+    <NavLink activeClassName="active" to="/signin">
+        <Text size="xl" color="white">Sign In
+        </Text>
+    </NavLink>
+
+    {/* Title and Logo */}
+    <NavLink activeClassName="active" to="/">
+    <Box display="flex" alignItems="center">
+    <Box margin={2} height={50} width={50}> 
+        <Image
+            alt="I-Dab Logo"
+            naturalHeight={1}
+            naturalWidth={1}
+            src="./icons/logo.svg"
+        />
+    </Box>  
+        <Heading size="xs" color="orange">
+            I-Dab
+        </Heading>
+    </Box>
+    </NavLink>
+    
+    {/* Sign Up Link */}
+    <NavLink activeClassName="active" to="/signup">
+        <Text size="xl" color="white">Sign Up
+        </Text>
+     </NavLink>
+
+    </Box>
+)
+
+export default Navbar; 
